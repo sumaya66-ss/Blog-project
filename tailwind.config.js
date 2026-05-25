8488559b0f158036c36ca13cb6c2634074879af8
SUMAYA <sa0468760@gmail.com>
2026-05-25 17:37:48 +0600

    first commir

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+        colors:{
+      brand:"#f8fafc",
+      accent:"#4f46e5",
+      card:"#ffffff",
+      accent2:"#f9172a",
+      mute:"#64748b",
+
+
+      },
+
+    },
+  },
+}
